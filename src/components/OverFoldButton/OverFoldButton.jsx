1d4ad5912ac27d94ed52dd0267469c00a39a904c
MkMonir <khanmonir1335@gmail.com>
2023-01-19 16:06:13 +0600

    over fold button component

---
+++ src/components/OverFoldButton/OverFoldButton.jsx
@@ -0,0 +1,14 @@
+import './overFoldButton.css';
+
+const OverFoldButton = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <button class="btn-31">
+        <span class="text-container">
+          <span class="text">Button</span>
+        </span>
+      </button>
+    </div>
+  );
+};
+export default OverFoldButton;

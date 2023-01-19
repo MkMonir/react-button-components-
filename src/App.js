04868ee248fa0e19aaa1eebb9efc904fef293250
MkMonir <khanmonir1335@gmail.com>
2023-01-19 16:04:09 +0600

    intialize react application

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import OverFoldButton from './components/OverFoldButton/OverFoldButton';
+import RippleButton from './components/RippleButton/RippleButton';
+
+function App() {
+  return (
+    <div className="App">
+      <RippleButton />
+      <OverFoldButton />
+    </div>
+  );
+}
+
+export default App;
